dd93f8ece8bac485987d3f6c7f37fda0e65268b0
CSelvidge <healincheese@gmail.com>
2024-06-17 08:17:13 -0400

    renamed main to gui because it will not be main

---
+++ gui/gui.py
@@ -0,0 +1,16 @@
+from tkinter import Tk, BOTH, Canvas
+
+class WINDOW:
+  
+    def __init__(width, height):
+        self.width = width
+        self.height = height
+
+    def redraw():
+        pass
+
+    def wait_for_close():
+        pass
+
+    def close():
+        pass
