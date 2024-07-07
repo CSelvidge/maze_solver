f1e7b42a95b0f738ae3776c21b0fa4b5d15102f5
CSelvidge <healincheese@gmail.com>
2024-07-07 09:03:57 -0400

    Continued work on cell creation optimization

--- gui/gui.py
+++ gui/gui.py
@@ -1,28 +1,51 @@
-from tkinter import Tk, BOTH, Canvas, Button, Frame, simpledialog
+from tkinter import LEFT,Tk, BOTH, Canvas, Button, Frame, simpledialog, Label
 import random
+import cProfile
 
 class Window:
-    def __init__(self, width, height, cell_size = None):
+    def __init__(self, width, height, cell_size = None, batch_size = 100):
         self.__root = Tk()
         self.__root.title("Maze Solver V 0.1")
         self.__canvas = Canvas(self.__root, width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=True)
         self.__running = False
         self.cell_size = cell_size
+        self.cell_count = 0
+        self.batch_size = batch_size #Only useful when trying batched optimization, otherwise unused
         self.cells = []
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
         self.create_buttons()
+        self.create_labels()
 
     def create_buttons(self) -> None:
         button_frame = Frame(self.__root)
         button_frame.pack(pady=10)
 
-        populate_button = Button(button_frame, text="Populate Canvas", command= self.populate_canvas)
-        populate_button.pack(padx=5)
+        populate_button = Button(button_frame, text="Populate Canvas", command= self.profiling_tests)
+        populate_button.pack(side = LEFT, padx=5)
 
         clear_button = Button(button_frame, text="Clear Canvas", command=self.clear_canvas)
-        clear_button.pack(padx=5)
+        clear_button.pack(side=LEFT, padx=5)
+
+        cell_size_button = Button(button_frame, text="Cell Size", command=self.user_cell_size)
+        cell_size_button.pack(side=LEFT, padx=5)
+
+        batch_size_button = Button(button_frame, text="Set Batch Size", command=self.user_batch_size)
+        batch_size_button.pack(side=LEFT, padx=5)
+
+    def create_labels(self) -> None:
+        self.count_label = Label(self.__root, text="Cell count:")
+        self.count_label.pack(side=LEFT, padx=5)
+
+        self.cell_size_label = Label(self.__root, text="Cell Size:")
+        self.cell_size_label.pack(side=LEFT, padx=5)
+
+    def update_count_label(self) -> None:
+        self.count_label.config(text=f"Cell Count: {self.cell_count}")
+
+    def update_cell_size_label(self) -> None:
+        self.cell_size_label.config(text=f"Cell Size: {self.cell_size}")
 
     def calculate_grid_size(self) -> None:
         if self.cell_size is None:
@@ -40,16 +63,28 @@ class Window:
         self.__canvas.delete("all")
         self.cells = []
 
+    def user_cell_size(self) -> None:
+        self.set_cell_size(simpledialog.askinteger("Input", "Enter new cell size(in pixels):", minvalue = 1))
+
     def set_cell_size(self, cell_size) -> None:
         self.cell_size = cell_size
+        self.update_cell_size_label()
+
+    def user_batch_size(self) -> None:
+        self.set_batch_size(simpledialog.askinteger("Input", "Enter batch size(in pixels):", minvalue=10))
+
+    def set_batch_size(self, batch_size) -> None:
+        self.batch_size = batch_size
 
     def populate_canvas(self) -> None:
         self.clear_canvas()
         if self.cell_size is None or self.cell_size < 1:
-            self.set_cell_size(simpledialog.askinteger("Input", "Enter cell size(in pixels):", minvalue=1))
+            self.set_cell_size(simpledialog.askinteger("Input", "Enter cell size(in pixels):", minvalue = 1))
         self.cell_geometry = self.calculate_grid_size()
         self.cells = self.populate_cells(self.cell_geometry[0], self.cell_geometry[1])
-        
+
+    def profiling_tests(self) -> None:
+        cProfile.runctx('self.populate_canvas()', globals(), locals(), filename='db.prof')
 
     def populate_cells(self, num_rows, num_cols) -> None:
         self.cells = [
@@ -57,8 +92,15 @@ class Window:
                 self.create_randomized_cell(col, row)
                 for col in range(num_cols)
             ]
-            for row in range(num_rows)
+           for row in range(num_rows)
         ]
+        self.cell_count = num_rows * num_cols
+        self.update_count_label()
+
+        self.draw_all_walls()
+
+        self.__root.update_idletasks()
+        self.__root.update() 
 
     def create_randomized_cell(self, col, row) -> 'Cell':
         x1 = col * self.cell_size
@@ -73,8 +115,18 @@ class Window:
             'bottom': random.choice([True,False]),
             'left': random.choice([True,False])
         }
-        cell.draw_walls()
+
         return cell
+    
+    def draw_all_walls(self) -> None:
+        drawn_walls = set()
+        lines_to_draw = []
+
+        for row in self.cells:
+            for cell in row:
+                lines_to_draw.extend(cell.draw_walls(drawn_walls))
+        
+        self.draw_lines(lines_to_draw)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -87,9 +139,15 @@ class Window:
 
     def close(self):
         self.__running = False
+        self.__root.destroy()
+
+    def draw_lines(self, lines:list) -> None:
+        for start, end in lines:
+            self.draw_line(Line(start, end), "black")
 
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
+        return line
 
 class Point:
     def __init__(self, x, y) -> None:
@@ -109,12 +167,7 @@ class Line:
 
 class Cell:
     def __init__(self, x1, y1, x2, y2, win) -> None:
-        self.walls = {
-            'top': True,
-            'right': True,
-            'bottom': True,
-            'left': True
-        }
+        self.walls = {}
 
         self._x1 = x1
         self._x2 = x2
@@ -123,20 +176,30 @@ class Cell:
 
         self._win = win
 
-    def draw_walls(self) -> None:
+    def draw_walls(self, drawn_walls) -> list:
+        walls_to_draw = []
 
-        walls = { #I love dictionaries
-            "left": Line(Point(self._x1, self._y1), Point(self._x1, self._y2)),
-            "right": Line(Point(self._x2, self._y2), Point(self._x2, self._y1)),
-            "top": Line(Point(self._x1, self._y1), Point(self._x2, self._y1)),
-            "bottom": Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-        }
+        top_wall = ((self._x1, self._y1), (self._x2, self._y1))
+        right_wall = ((self._x2, self._y1), (self._x2, self._y2))
+        bottom_wall = ((self._x1, self._y2), (self._x1, self._y2))
+        left_wall = ((self._x1, self._y1), (self._x1, self._y2))
 
-        for wall, is_present in self.walls.items():
-            if is_present:
-                self._win.draw_line(walls[wall], "black")
+        if self.walls.get('top') and top_wall not in drawn_walls:
+            walls_to_draw.append((Point(self._x1, self._y1), Point(self._x2, self._y1)))
+            drawn_walls.add(top_wall)
+        if self.walls.get('right') and right_wall not in drawn_walls:
+            walls_to_draw.append((Point(self._x2, self._y1), Point(self._x2, self._y2)))
+            drawn_walls.add(right_wall)
+        if self.walls.get('bottom') and bottom_wall not in drawn_walls:
+            walls_to_draw.append((Point(self._x1, self._y2), Point(self._x2, self._y2)))
+            drawn_walls.add(bottom_wall)
+        if self.walls.get('left') and left_wall not in drawn_walls:
+            walls_to_draw.append((Point(self._x1, self._y1), Point(self._x1, self._y2)))
+            drawn_walls.add(left_wall)
 
-    def draw_move(self, cell, undo = False) -> None:
+        return walls_to_draw
+
+    def draw_move(self, cell, undo: bool = False) -> None:
         if undo:
             self._draw_color = "gray"
         else:
@@ -145,4 +208,4 @@ class Cell:
         self._center = Point(((self._x1 + self._x2) / 2), (self._y1 + self._y2) / 2)
         _cell_center = Point(((cell._x1 + cell._x2) / 2), (cell._y1 + cell._y2) / 2)
         line = Line(self._center, _cell_center)
-        self._win.draw_line(line, self._draw_color)       
+        self._win.draw_line(line, self.__draw_color)       
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from gui.gui import *
 
+
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(50, 50, 100, 100, win)
-    cell2 = Cell(110, 50, 160, 100, win)
-
-    cell1.draw_walls()
-    cell2.draw_walls()
-
-    cell1.draw_move(cell2, True)
-
-
-
     win.wait_for_close()
-
 
 
 if __name__ == "__main__":
